fa73905b4f69f9f59081ea7667754537bbab6d65
ShunYamada <sy@gociao.com>
2018-02-16 19:14:51 +0900

    Image Picker

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -1,11 +1,53 @@
 
 import React from 'react';
-import { View, Text, Picker } from 'react-native';
+import { View, Text, Picker, TouchableOpacity, PixelRatio } from 'react-native';
 import { connect } from 'react-redux';
 import { employeeUpdate } from '../actions';
 import { CardSection, Input } from './common';
+import ImagePicker from 'react-native-image-picker';
 
 class EmployeeForm extends React.Component {
+  state = {
+    ImageSource: null,
+  };
+
+  selectPhotoTapped() {
+    const options = {
+      quality: 1.0,
+      maxWidth: 500,
+      maxHeight: 500,
+      storageOptions: {
+        skipBackup: true
+      }
+    };
+
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled photo picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        let source = { uri: response.uri };
+
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+        this.setState({
+
+          ImageSource: source
+
+        });
+      }
+    });
+  }
+
   render() {
     return (
       <View>
@@ -43,21 +85,47 @@ class EmployeeForm extends React.Component {
             <Picker.Item label="Sunday" value="Sunday" />
           </Picker>
         </CardSection>
+
+        <CardSection>
+          <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
+            <View style={styles.ImageContainer}>
+            { this.state.ImageSource === null ? <Text>Select a Photo</Text> :
+              <Image style={styles.ImageContainer} source={this.state.ImageSource} />
+            }
+            </View>
+          </TouchableOpacity>
+        </CardSection>
       </View>
     );
   }
-}
+};
+
 
 const styles = {
   pickerTextStyle: {
     fontSize: 18,
     paddingLeft: 20
-  }
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF8E1'
+  },
+  ImageContainer: {
+    borderRadius: 10,
+    width: 250,
+    height: 250,
+    borderColor: '#9B9B9B',
+    borderWidth: 1 / PixelRatio.get(),
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#CDDC39',
+  },
 };
 
 const mapStateToProps = (state) => {
   const { name, phone, shift } = state.employeeForm;
-
   return { name, phone, shift };
 };
 
